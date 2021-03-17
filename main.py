43919986640d98230a6f01927a924d79f64231bf
martin <mail@mama.sh>
2021-03-17 11:13:40 +0100

    looks at images

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import imutils as util
-
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
 
 def extractCard(aprox,img):
     pts1 = aprox.reshape(4, 2)
@@ -19,54 +14,68 @@ def extractCard(aprox,img):
     rect[2] = pts1[np.argmax(diff)]
 
     pts2 = np.float32([[0, 0], [200, 0], [0, 300], [200, 300]])
-    matrix = cv2.getPerspectiveTransform(rect, pts2)
-    result = cv2.warpPerspective(img, matrix, (200, 300))
+    matrix = cv.getPerspectiveTransform(rect, pts2)
+    result = cv.warpPerspective(img, matrix, (200, 300))
     return result
+    
 
 def extractCornor(card):
-
     cornor = card[0:70,1:25]
-
-    cornor = cv2.cvtColor(cornor,cv2.COLOR_RGB2GRAY)
-
-    _, cornor = cv2.threshold(cornor, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-
+    cornor = cv.cvtColor(cornor,cv.COLOR_RGB2GRAY)
+    _, cornor = cv.threshold(cornor, 0, 255, cv.THRESH_OTSU | cv.THRESH_BINARY_INV)
     return cornor
+
 
 def saveCornor(cornor):
     card = input("Card ID: ")
-    cv2.imwrite(f"Resources/{card}.png",cornor)
+    cv.imwrite(f"Resources/{card}.png",cornor)
 
-h4 = cv2.imread("Resources/1.png")
-h4 = cv2.resize(h4,(70,24))
-
-while True:
-    success, img = cap.read()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    squere = cv2.Canny(gray,150,150)
+def findCard(img):
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    squere = cv.Canny(gray,150,150)
     k = np.ones([2,2],np.uint8)
-    squere = cv2.dilate(squere,k,iterations=2)
+    squere = cv.dilate(squere,k,iterations=2)
 
-    see = cv2.findContours(squere.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    see = cv.findContours(squere.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
     see = util.grab_contours(see)
-    see = sorted(see,key=cv2.contourArea,reverse=True)[:3]
+    see = sorted(see,key=cv.contourArea,reverse=True)[:3]
 
     for s in see:
-        lnked = cv2.arcLength(s,True)
-        aprox = cv2.approxPolyDP(s,0.02*lnked,True)
+        lnked = cv.arcLength(s,True)
+        aprox = cv.approxPolyDP(s,0.02*lnked,True)
 
         if len(aprox) == 4:
 
             result = extractCard(aprox,img)
             cornor = extractCornor(result)
 
-            score, diff = from
-
-            cv2.imshow("Cornor", cornor)
+            cv.imshow(f"Cornor{s}", cornor)
             #saveCornor(cornor)
-            cv2.imshow("Perspective",result)
-            cv2.drawContours(img,[aprox],-1,(0,255,0),2)
+            cv.imshow(f"Perspective{s}",result)
+            cv.drawContours(img,[aprox],-1,(0,255,0),2)
 
-    cv2.imshow("Video",img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv.imshow("Video",img)
+
+def useWebCam():
+    cap = cv.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    cap.set(10,100)
+
+    while True:
+        success, img = cap.read()
+        findCard(img)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+def useImage(imagePath):
+    img = cv.imread(imagePath)
+    findCard(img)
+
+if __name__ == '__main__':
+    h4 = cv.imread("Resources/1.png")
+    h4 = cv.resize(h4,(70,24))
+    useImage('Resources/imgg.jpeg')
+    cv.waitKey()
+    cv.destroyAllWindows()
+
